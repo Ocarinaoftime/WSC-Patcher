dd0134c74daa1a29c20c7629a9908d0c9137c7f3
Ocarinaoftime <89474990+Ocarinaoftime@users.noreply.github.com>
2024-10-11 19:27:04 -0400

    Update patch_ec_title_check.go to not set all titles and tickets as managed

--- patch_ec_title_check.go
+++ patch_ec_title_check.go
@@ -25,31 +25,6 @@ var NegateECTitle = PatchSet{
 			}.Bytes(),
 		},
 		{
-			Name:     "Mark all titles as managed",
-			AtOffset: 620656,
-
-			Before: Instructions{
-				STWU(R1, R1, 0xfff0),
-				MFSPR(R0, LR),
-			}.Bytes(),
-			After: Instructions{
-				LI(R3, 1),
-				BLR(),
-			}.Bytes(),
-		},
-		{
-			Name:     "Mark all tickets as managed",
-			AtOffset: 619904,
-			Before: Instructions{
-				STWU(R1, R1, 0xfff0),
-				MFSPR(R0, LR),
-			}.Bytes(),
-			After: Instructions{
-				LI(R3, 1),
-				BLR(),
-			}.Bytes(),
-		},
-		{
 			Name:     "Nullify ec::removeAllTitles",
 			AtOffset: 588368,
 			Before: Instructions{
